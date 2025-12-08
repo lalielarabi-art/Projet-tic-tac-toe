d3a6c9c6c575564b55fbe0e0cf88c1c07e5479b5
lalielarabi-art <lalielarabi@gmail.com>
2025-12-08 11:20:53 +0100

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "game.hpp"
+#include "board.hpp"
+#include "Player.hpp"
+
+
+void turn(Player& player, char plateau[][3]){
+
+      std::cout<<"Le joueur "<<player.name<<" doit jouer."<<std::endl;
+
+      int box;
+      std::cin >> box;
+
+      int lign {(box-1)/3};
+      int column {(box-1)%3};
+
+      plateau[lign][column]=player.symbol;
+
+      draw_game_board(plateau);
+    }
+
+    bool sby_won(char plateau[][3]){
+
+     /*/Verif des colonnes*/
+    for (int i{0}; i<3; ++i){
+        if(plateau[i][0]==plateau[i][1] and plateau[i][1]==plateau[i][2]){
+        return true;
+        }
+    }
+    /*/Verif des lignes*/
+    for (int j{0}; j<3; ++j){
+        if(plateau[0][j]==plateau[1][j] and plateau[1][j]==plateau[2][j]){
+        return true;
+        }
+    }
+    /*/Verif des diagos*/
+    if (plateau[0][0]==plateau[1][1] and plateau[1][1]==plateau[2][2]){
+        return true;
+    }
+    if (plateau[0][2]==plateau[1][1] and plateau[1][1]==plateau[2][0]){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
---
+++ game.hpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include "Player.hpp"
+
+void turn(Player& player, char plateau[][3]);
+
+bool sby_won(char plateau[][3]);
