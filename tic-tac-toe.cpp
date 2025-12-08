09ba364bbdb1eba5d79e02e76057763ec6bff230
lalielarabi-art <lalielarabi@gmail.com>
2025-12-08 10:21:20 +0100

    Update tic-tac-toe.cpp

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -14,7 +14,15 @@ int main() {
       {'4','5','6'},
       {'7','8','9'}
     };
-
+    
+    int box {};
+    
+    draw_game_board(plateau);
+    std::cout<<"Le joueur "<<player1.name<<" doit jouer."<<std::endl;
+    std::cin >> box;
+    int lign {(box-1)/3};
+    int column {(box-1)%3};
+    plateau[lign][column]=player1.symbol;
     draw_game_board(plateau);
     
     return 0;
