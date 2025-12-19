8563557a5448fc94b6d005e8907db0c48c2a1c8f
lalielarabi-art <lalielarabi@gmail.com>
2025-12-19 18:45:49 +0100

    Add block function and update sby_won declaration

--- game.hpp
+++ game.hpp
@@ -2,5 +2,7 @@
 #include "Player.hpp"
 
 void turn(Player& player, char plateau[][3]);
+void ia_turn(Player& ia, Player& player, char plateau[][3]);
 
 bool sby_won(char plateau[][3]);
+int block(char symbol_player, char plateau[][3]);
